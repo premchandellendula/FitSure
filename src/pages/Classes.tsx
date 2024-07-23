cbf507be5ffe53cb1d4f40050486b12f79fd266e
premchandellendula <premcellendula@gmail.com>
2024-07-23 16:10:44 +0530

    pricing and classes

--- src/pages/Classes.tsx
+++ src/pages/Classes.tsx
@@ -1,7 +1,111 @@
 import { TopBanner } from "../components/Banner/TopBanner"
+import cyclingImg from "../images/asset 47.jpeg"
+import KarateImg from "../images/asset 48.jpeg"
+import liftingImg from "../images/asset 49.jpeg"
+import meditationImg from "../images/asset 50.jpeg"
+import boxingImg from "../images/asset 51.jpeg"
+import workoutImg from "../images/asset 52.jpeg"
+import crossfitImg from "../images/asset 16.jpeg"
+import runningImg from "../images/asset 17.jpeg"
+import { Link } from "react-router-dom"
+import Footer from "../components/Footer/Footer"
 
 export const Classes = () => {
+    const classList = [
+        {
+            img: cyclingImg,
+            className: "Cycling",
+            coach: "Pooja Hegde",
+            day: "Tue",
+            time: "7:00 AM"
+        },
+        {
+            img: KarateImg,
+            className: "Karate",
+            coach: "Pawan Kalyan",
+            day: "Wed",
+            time: "6:00 PM"
+        },
+        {
+            img: liftingImg,
+            className: "Power Lifting",
+            coach: "Rana Daggubati",
+            day: "Sat",
+            time: "7:00 PM"
+        },
+        {
+            img: meditationImg,
+            className: "Meditation",
+            coach: "Samantha",
+            day: "Mon",
+            time: "7:00 AM"
+        },
+        {
+            img: boxingImg,
+            className: "Boxing",
+            coach: "Ravi Teja",
+            day: "Thurs",
+            time: "6:00 PM"
+        },
+        {
+            img: workoutImg,
+            className: "Workout",
+            coach: "Ram Charan",
+            day: "Sune",
+            time: "7:00 AM"
+        },
+        {
+            img: crossfitImg,
+            className: "Crossfit",
+            coach: "Allu Arjun",
+            day: "Fri",
+            time: "9:00 PM"
+        },
+        {
+            img: runningImg,
+            className: "Running",
+            coach: "NTR",
+            day: "Wed",
+            time: "7:30 AM"
+        }
+    ]
     return <div>
         <TopBanner page={"Classes"} />
+        <div className="grid grid-cols-3 w-[85%] m-auto gap-y-10 my-20">
+            {classList.map((classes, idx) => <ClassCard key={idx} img={classes.img} className={classes.className} coach={classes.coach} day={classes.day} time={classes.time} />)}
+        </div>
+        <Footer />
     </div>
+}
+
+interface ClassCardInputs {
+    img: string,
+    className: string,
+    coach: string,
+    day: string,
+    time: string
+}
+
+function ClassCard({img, className, coach, day, time}: ClassCardInputs){
+    return <div className="relative border w-[90%] h-[25rem] m-auto rounded-3xl shadow-2xl">
+            <div className="w-full h-full">
+                <img src={img} alt="" className="object-cover h-full w-full rounded-3xl" style={{ filter: 'blur(0.5px)' }}/>
+            </div>
+
+            <div className="absolute inset-0">
+                <h2 className="absolute text-white text-2xl font-semibold left-10 bottom-32 top-[53%]">{className}</h2>
+                <span className="absolute w-10 h-1 bg-yellow-500 mt-2 left-10 top-[60%]"></span>
+                <div className="flex absolute left-10 top-[67%] gap-5 text-lg">
+                    <p className="text-white">
+                        <i className='bx bx-user-circle'></i> {coach}
+                    </p>
+                    <p className="text-white">
+                        <i className='bx bx-time-five' ></i> {day} : {time}
+                    </p>
+                </div>
+                <Link to="/contact">
+                    <button className="text-black bg-white hover:bg-gray-200 focus:outline-none font-medium text-lg px-5 py-2.5 uppercase absolute left-10 top-[80%]">Take a Tour</button>
+                </Link>
+            </div>
+        </div> 
 }
--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,7 +1,65 @@
 import { TopBanner } from "../components/Banner/TopBanner"
+import beginnerImg from "../.../../images/asset 41.jpeg"
+import intermediateImg from "../.../../images/asset 42.jpeg"
+import proImg from "../.../../images/asset 43.jpeg"
+import Footer from "../components/Footer/Footer"
 
 export const Pricing = () => {
+    const pricingCard = [
+        {
+            img: beginnerImg,
+            name: "Beginner",
+            cost: "499",
+
+        },
+        {
+            img: intermediateImg,
+            name: "Intermediate",
+            cost: "899",
+            
+        },
+        {
+            img: proImg,
+            name: "Pro",
+            cost: "1299",
+            
+        }
+    ]
     return <div>
         <TopBanner page={"Pricing"} />
+        <div className="bg-gray-100 py-20">
+            <div className="flex flex-col justify-center items-center">
+                <h2 className="text-4xl font-semibold my-5">Exclusice Pricing Plan</h2>
+                <p className="text-lg font-normal text-gray-600">With an affordable pricing, make sure you hit the gym and sweat it out.</p>
+            </div>
+            <div className="flex justify-between mt-10 w-[85%] m-auto">
+                {pricingCard.map((pricing, idx) => <PricingCard key={idx} img={pricing.img} name={pricing.name} cost={pricing.cost} />)}
+            </div>
+        </div>
+        <Footer />
+    </div>
+}
+
+interface PricingInputs {
+    img: string,
+    name: string,
+    cost: string
+}
+
+function PricingCard({img, name, cost}: PricingInputs){
+    return <div className="bg-white shadow">
+        <div className="flex flex-col justify-center items-center">
+            <img src={img} alt="" className="" />
+            <h2 className="text-2xl font-semibold my-4">{name}</h2>
+            <p className="text-3xl font-bold my-2"><span className="text-gray-400">&#8377;</span> {cost} <span className="text-lg text-gray-400">p/m</span></p>
+            <div className="text-lg text-gray-600 p-2 text-center flex flex-col gap-5 mb-2">
+                <p>Free Hand</p>
+                <p>Gym Fitness</p>
+                <p>Weight Loss</p>
+                <p>Personal Trainer</p>
+                <p>Cycling</p>
+            </div>
+            <button className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none font-medium rounded-full text-lg px-5 py-2.5 uppercase my-4">Purchase Now</button>
+        </div>
     </div>
 }
