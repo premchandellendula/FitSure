17a3c0b4e9aa785572c4d8585ce57448ba3a457b
premchandellendula <premcellendula@gmail.com>
2024-07-25 10:59:43 +0530

    topbanner signup and signin

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,3 +1,4 @@
+import { TopBanner } from "../components/Banner/TopBanner"
 import { BottomWarning } from "../components/Signup/BottomWarning"
 import { Button } from "../components/Signup/Button"
 import { Heading } from "../components/Signup/Heading"
@@ -5,20 +6,27 @@ import { InputBox } from "../components/Signup/InputBox"
 import { SubHeading } from "../components/Signup/SubHeading"
 
 export const Signin = () => {
+    return <div>
+        <TopBanner page={"Sign In"} />
+        <SignIn />
+    </div>
+}
+
+function SignIn(){
     return <div className='bg-yellow-50 h-screen flex justify-center'>
-    <div className='flex flex-col justify-center'>
-        <div className='rounded-lg bg-white w-80 text-center p-2 h-max px-4'>
-            <Heading label={"Sign In"}/>
-            <SubHeading label={"Enter your credentials to access your account"}/>
-            <InputBox label={"Email"} placeholder={"johndoe@gmail.com"}/>
+        <div className='flex flex-col justify-center'>
+            <div className='rounded-lg bg-white w-80 text-center p-2 h-max px-4'>
+                <Heading label={"Sign In"}/>
+                <SubHeading label={"Enter your credentials to access your account"}/>
+                <InputBox label={"Email"} placeholder={"johndoe@gmail.com"}/>
 
-            <InputBox label={"Password"} placeholder={"123abc"}/>
+                <InputBox label={"Password"} placeholder={"123abc"}/>
 
-            <div className='pt-4'>
-                <Button label={"Sign In"} />
+                <div className='pt-4'>
+                    <Button label={"Sign In"} />
+                </div>
+                <BottomWarning label={"Don't have an account?"} buttonText={"Sign Up"} to={'/signup'}/>
             </div>
-            <BottomWarning label={"Don't have an account?"} buttonText={"Sign Up"} to={'/signup'}/>
         </div>
     </div>
-</div>
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,3 +1,4 @@
+import { TopBanner } from "../components/Banner/TopBanner"
 import { BottomWarning } from "../components/Signup/BottomWarning"
 import { Button } from "../components/Signup/Button"
 import { Heading } from "../components/Signup/Heading"
@@ -5,9 +6,16 @@ import { InputBox } from "../components/Signup/InputBox"
 import { SubHeading } from "../components/Signup/SubHeading"
 
 export const Signup = () => {
+    return <div>
+        <TopBanner page={"Sign Up"} />
+        <SignUp />
+    </div>
+}
+
+function SignUp(){
     return <div className="bg-yellow-50 h-screen flex justify-center">
         <div className="flex flex-col justify-center">
-        <div className='rounded-lg bg-white w-80 text-center p-2 h-max px-4'>
+            <div className='rounded-lg bg-white w-80 text-center p-2 h-max px-4'>
                 <Heading label={"Sign Up"}/>
                 <SubHeading label={"Enter your information to create an account"}/>
                 <InputBox label={"Name"} placeholder='John Doe' />
