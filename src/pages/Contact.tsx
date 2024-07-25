977c8e8b6f56ead96db018bba616604b28e0ac89
premchandellendula <premcellendula@gmail.com>
2024-07-25 11:08:22 +0530

    camelCase Error in Contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -94,6 +94,14 @@ function ContactDiv(){
 
 function Map(){
     return <div className="my-20">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d487277.8589203762!2d78.0799244159689!3d17.41858790068452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb99daeaebd2c7%3A0xae93b78392bafbc2!2sHyderabad%2C%20Telangana!5e0!3m2!1sen!2sin!4v1721742843016!5m2!1sen!2sin" width="600" height="350" style={{ width: "100%", height: "30rem", border: "0px" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d487277.8589203762!2d78.0799244159689!3d17.41858790068452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb99daeaebd2c7%3A0xae93b78392bafbc2!2sHyderabad%2C%20Telangana!5e0!3m2!1sen!2sin!4v1721742843016!5m2!1sen!2sin" 
+        width="600" 
+        height="350" 
+        style={{ width: "100%", height: "30rem", border: "0px" }} 
+        allowFullScreen 
+        loading="lazy" 
+        referrerPolicy="no-referrer-when-downgrade">
+        </iframe>
     </div>
 }
